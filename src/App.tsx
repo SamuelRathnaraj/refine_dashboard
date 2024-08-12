43780eea8169901086cf9c6ffa068a3b10892a44
SamuelRathnaraj <samvelrathnaraj23@gmail.com>
2024-08-12 20:07:09 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,94 @@
+import { Authenticated, GitHubBanner, Refine } from "@refinedev/core";
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
+import { useNotificationProvider } from "@refinedev/antd";
+import "@refinedev/antd/dist/reset.css";
+
+import { authProvider, dataProvider, liveProvider } from "./providers";
+import { Home, ForgotPassword, Login, Register, CompanyList } from "./pages";
+import routerBindings, {
+  CatchAllNavigate,
+  DocumentTitleHandler,
+  UnsavedChangesNotifier,
+} from "@refinedev/react-router-v6";
+import { App as AntdApp } from "antd";
+// import { createClient } from "graphql-ws";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import Layout from "./components/layout";
+import { resources } from "./config/resources";
+import Create from "./pages/company/create";
+import EditPage from "./pages/company/edit";
+import List from "./pages/tasks/list";
+import CreateTask from "./pages/tasks/Create";
+import EditTask from "./pages/tasks/edit";
+import TasksCreatePage from "./pages/tasks/Create";
+import TasksEditPage from "./pages/tasks/edit";
+
+// const API_URL = "https://api.nestjs-query.refine.dev/graphql";
+// const WS_URL = "wss://api.nestjs-query.refine.dev/graphql";
+
+// const gqlClient = new GraphQLClient(API_URL);
+// const wsClient = createClient({ url: WS_URL });
+
+function App() {
+  return (
+    <BrowserRouter>
+      <GitHubBanner />
+      <RefineKbarProvider>
+          <AntdApp>
+            <DevtoolsProvider>
+              <Refine
+                dataProvider={dataProvider}
+                liveProvider={liveProvider}
+                notificationProvider={useNotificationProvider}
+                routerProvider={routerBindings}
+                authProvider={authProvider}
+                resources={resources}
+                options={{
+                  syncWithLocation: true,
+                  warnWhenUnsavedChanges: true,
+                  useNewQueryKeys: true,
+                  projectId: "s32sTe-CXzlpv-qd9uU7",
+                  liveMode: "auto",
+                }}
+              >
+                <Routes>
+                  <Route path="/register" index element ={<Register />}/>
+                  <Route path="/login" index element ={<Login />}/>
+                  <Route path="/forgot-password" index element ={<ForgotPassword />}/>
+                  <Route element={<Authenticated key="authenticated-layout"
+                  fallback={<CatchAllNavigate to="/login" />}
+                  >
+                      <Layout>
+                        <Outlet />
+                      </Layout>
+                    </Authenticated>
+                  }>
+                     <Route index element ={<Home />}/>
+                     <Route path="/companies" >
+                        <Route index element={<CompanyList/>}/>
+                        <Route path="new" element={<Create/>} />
+                        <Route path="edit/:id" element={<EditPage/>} />
+                     </Route>
+                     <Route path="/tasks" element={<List>
+                        <Outlet/>
+                     </List>}>
+                     <Route path="new" element={<TasksCreatePage/>}/>
+                     <Route path="edit/:id" element={<TasksEditPage/>}/>
+                     </Route>
+                  </Route>
+                </Routes>
+                <RefineKbar />
+                <UnsavedChangesNotifier />
+                <DocumentTitleHandler />
+              </Refine>
+              <DevtoolsPanel />
+            </DevtoolsProvider>
+          </AntdApp>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
